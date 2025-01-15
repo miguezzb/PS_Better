cffdb15009811802daf564516b4d6cf15b4c841c
Dell <Dell@DESKTOP-07842MO>
2025-01-15 10:23:05 -0600

    Agregar archivos de proyecto.

---
+++ ParkingSystem/Controllers/Usuarios.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ParkingSystem.Controllers
+{
+    public class Usuarios : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Logut()
+        {
+            return View();
+        }
+    }
+}
