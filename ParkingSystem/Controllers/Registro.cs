2b387813ce03712d12c6b89932ffc56d5650d3da
Dell <Dell@DESKTOP-07842MO>
2025-01-15 13:07:06 -0600

    login registro, cambios en las col de usuarios

---
+++ ParkingSystem/Controllers/Registro.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ParkingSystem.Controllers
+{
+    public class Registro : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
