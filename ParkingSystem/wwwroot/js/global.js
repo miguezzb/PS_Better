dde71d11807874fe10f7e837ceb338545279e797
Dell <Dell@DESKTOP-07842MO>
2025-01-17 13:53:26 -0600

    View 'Panel' ya funciona, Modal para cargar ya funciona

---
+++ ParkingSystem/wwwroot/js/global.js
@@ -0,0 +1,11 @@
+﻿// Mostrar el modal
+function mostrarModal() {
+    var modal = new bootstrap.Modal(document.getElementById('staticBackdrop'));
+    modal.show();
+}
+
+// Ocultar el modal
+function ocultarModal() {
+    var modal = bootstrap.Modal.getInstance(document.getElementById('staticBackdrop'));
+    modal.hide();
+}
