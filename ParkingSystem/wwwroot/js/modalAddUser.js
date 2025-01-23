c36909968f88bd10c14e006d0fc7e44fd771b3f2
Dell <Dell@DESKTOP-07842MO>
2025-01-23 17:40:14 -0600

    muchas cosas agregadas

---
+++ ParkingSystem/wwwroot/js/modalAddUser.js
@@ -0,0 +1,15 @@
+﻿var modalElement = document.getElementById('addUserModal');
+
+// Mostrar el modal
+function mostrarAddUserModal() {
+    var modal = bootstrap.Modal.getInstance(modalElement) || new bootstrap.Modal(modalElement);
+    modal.show();
+}
+
+// Ocultar el modal
+function ocultarAddUserModal() {
+    var modal = bootstrap.Modal.getInstance(modalElement);
+    if (modal) {
+        modal.hide();
+    }
+}
